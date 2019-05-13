ffb99170f167cbb60d2e865e1b27533234a04c6f
Marcus Vinícius <marcus.ah@gmail.com>
2019-05-13 17:58:12 +0000

    Validação de E-mail e CPF

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,3 +1,9 @@
 class Cliente < ApplicationRecord
     has_many :animal, dependent: :destroy
+    VALID_EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    validates :email, presence: true, length: { minimum: 5 },
+        format: { with: VALID_EMAIL }, uniqueness: true
+    VALID_CPF = /[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/
+    validates :cpf, presence: true,
+        format: {with: VALID_CPF}, uniqueness: true
 end
