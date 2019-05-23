716fe1ccdb4714a0516505e38f5113b85d8706f1
Jordana <jordana.ferreira05@gmail.com>
2019-05-23 01:42:25 +0000

    Validação de campos obrigatórios da tabela cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,5 +1,11 @@
 class Cliente < ApplicationRecord
     has_many :animal, dependent: :destroy
+    
+    validates_presence_of :nome, message: 'Não pode ser deixado em branco'
+    validates_presence_of :email, message: 'Não pode ser deixado em branco'
+    validates_presence_of :telefone, message: 'não pode ser deixado em branco'
+    
+      
     VALID_EMAIL = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     validates :email, presence: true, length: { minimum: 5 },
         format: { with: VALID_EMAIL }, uniqueness: true
