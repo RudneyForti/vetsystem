d64866418fe929e42ff892df6195a2d896f5ca01
Jordana <jordana.ferreira05@gmail.com>
2019-05-23 01:39:53 +0000

    Validação de campos obrigatórios na tabela animal

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,4 +1,9 @@
 class Animal < ApplicationRecord
   belongs_to :cliente
   has_many :atendimento, dependent: :destroy
+  
+  validates_presence_of :nome, message: 'não pode ser deixado em branco'
+  validates_presence_of :especie, message: 'não pode ser deixado em branco'
+  validates_presence_of :raca, message: 'Não pode ser deixado em branco'
+
 end
