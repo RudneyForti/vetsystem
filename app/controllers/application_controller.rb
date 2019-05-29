8ef9ece4d15a539b37cba75bc594ada8b07ae5eb
Marcus Vinicius <thepsn96@gmail.com>
2019-05-29 18:26:41 +0000

    Adição de Username no devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
     
     before_action :authenticate_user!
+    
+    before_action :configure_permitted_parameters, if: :devise_controller?
+protected
+        def configure_permitted_parameters
+             devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+             devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
+             devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+        end
+        
+    protect_from_forgery with: :exception
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_115615) do
+ActiveRecord::Schema.define(version: 2019_05_29_181748) do
 
   create_table "animals", force: :cascade do |t|
     t.string "nome"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2019_05_22_115615) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
