0c5ef7f51ed2ba6465a6fb5f9c2fa3a72a1ed57e
Marcus Vinicius <thepsn96@gmail.com>
2019-05-29 12:05:34 +0000

    Alteração no devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,17 @@
 class RegistrationsController < ApplicationController
     
-    def inicio
-    end
+private
+
+  def sign_up_params
+
+    params.require(:user).permit(:first_name, :email, :password, :password_confirmation)
+
+  end
+
+  def account_update_params
+
+    params.require(:user).permit(:first_name, :email, :password, :password_confirmation, :current_password)
+
+  end
     
 end
--- db/migrate/20190522115615_devise_create_users.rb
+++ db/migrate/20190522115615_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      #t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -35,7 +36,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
 
       t.timestamps null: false
     end
-
+    
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
