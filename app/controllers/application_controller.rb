10eb54d2cf1bfd90c9f2ecd49bfe64e769ce0bfa
Marcus Vinicius <thepsn96@gmail.com>
2019-05-30 19:56:06 +0000

    Segunda atualização de devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
     
     before_action :authenticate_user!
     
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :configure_permitted_parameters, if: :devise_controller? 
 protected
         def configure_permitted_parameters
-             devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-             devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
-             devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+             devise_parameter_sanitizer.permit(:sign_up, keys: [:username]) #permissão de username para cadastro
+             devise_parameter_sanitizer.permit(:sign_in, keys: [:username]) #permissão de username para logar
+             devise_parameter_sanitizer.permit(:account_update, keys: [:username]) #atualização da conta
         end
         
     protect_from_forgery with: :exception
