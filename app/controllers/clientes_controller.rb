0e37500f73e14f40b08bf7f1d3d67dd0db9efe29
Jordana <jordana.ferreira05@gmail.com>
2019-06-02 16:38:49 +0000

    Ordenar elementos da tabela animal e clientes pelo nome

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -4,7 +4,7 @@ class ClientesController < ApplicationController
   # GET /clientes
   # GET /clientes.json
   def index
-    @clientes = Cliente.all.all.page(params[:page]).per(4)
+    @clientes = Cliente.order("nome").page(params[:page]).per(4)
   end
 
   # GET /clientes/1
